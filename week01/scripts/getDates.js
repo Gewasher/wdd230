8e82c7423d34c4a288605164dc94de7fd86d053d
Gewasher <1010gmoney@gmail.com>
2024-06-15 14:16:27 -0600

    siteplan-first

---
+++ week01/scripts/getDates.js
@@ -0,0 +1,6 @@
+let curentDate = new Date();
+let year = curentDate.getFullYear();
+
+document.querySelector('#year').innerHTML = year;
+
+document.querySelector('#lastModified').innerHTML = `Last Modified: ${document.lastModified}`;
