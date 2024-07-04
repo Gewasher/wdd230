ec4915b779f6dbd2e85304ac93a0d46973f39151
Gewasher <1010gmoney@gmail.com>
2024-07-03 19:47:17 -0600

    w4

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,6 +1,9 @@
 let curentDate = new Date();
 let year = curentDate.getFullYear();
 
+
 document.querySelector('#year').innerHTML = year;
 
 document.querySelector('#lastModified').innerHTML = `Last Modified: ${document.lastModified}`;
+
+document.querySelector('#timestamp').value = curentDate
